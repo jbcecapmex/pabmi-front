a9e5e8e36bef78b15c6c006499d79802f39364a0
Mayra Cortés <rvillarreal@cecapmex.com>
2023-04-24 11:48:15 -0600

    roles

--- src/componentes/configuracion/Roles/Roles.tsx
+++ src/componentes/configuracion/Roles/Roles.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Button, Box, Grid } from "@mui/material";
+import { Button, Box, Grid, Link } from "@mui/material";
+import Breadcrumbs from "@mui/material";
 
 export default function Principal() {
   return (
     <Grid container>
-Roles
+h
     </Grid>
   );
 }
